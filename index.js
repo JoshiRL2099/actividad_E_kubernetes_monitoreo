cce2a169c79a25fdb0eee4c9cd3aeede9ba21258
JoshiRL <apato_2003@yahoo.com.mx>
2026-03-26 20:50:31 -0600

    initial-commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/method1', (req, res) => {
+  res.json({ message: 'Respuesta del método 1' });
+});
+
+app.get('/api/method2', (req, res) => {
+  res.json({ message: 'Respuesta del método 2' });
+});
+
+app.get('/api/method3', (req, res) => {
+  res.json({ message: 'Respuesta del método 3' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
+
+module.exports = app;
